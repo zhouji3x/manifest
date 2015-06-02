871d74b16fde4e25cd9d11e1fa1aec123566e693
Stefan Stanacar <stefan.stanacar@intel.com>
2015-06-02 17:39:05 +0300

    Temporary topic branches
    Resetting the patch to latest master as of June 3rd
    
    Change-Id: I00bc8ab18a69a973839318eb67363fef6a8b3575
    Signed-off-by: Stefan Stanacar <stefan.stanacar@intel.com>
    Reviewed-on: https://android.intel.com:443/373857

---
+++ topic_updater.sh
@@ -0,0 +1,15 @@
+for topic in 01org aosp art audio autodetect boot-devel camera comms graphics-gen sensors system-integration telephony
+do
+    mkdir -p topic/$topic
+    repo manifest -o topic/$topic/master
+    sed -i "s|android/l/mr1/r1|abt/topic/gmin/l-dev/mr1/$topic/master|g" topic/$topic/master
+    sed -i "s|android/l/mr1/master|abt/topic/gmin/l-dev/mr1/$topic/master|g" topic/$topic/master
+done
+
+for topic in scale bxt
+do
+    mkdir -p topic/staging/$topic
+    repo manifest -o topic/staging/$topic/master
+    sed -i "s|android/l/mr1/r1|abt/topic/gmin/staging/$topic/master|g" topic/staging/$topic/master    
+    sed -i "s|android/l/mr1/master|abt/topic/gmin/staging/$topic/master|g" topic/staging/$topic/master
+done
